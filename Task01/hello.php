6be3f5ac9b155ffee20c7fd96626506d454250af
openeeer <dfxminecraft@gmail.com>
2025-02-04 14:51:41 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,6 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+$dateTime = date("d.m.Y H:i:s");
+$FIO = 'Шеянов Александр';
+echo "Текущая дата и время: $dateTime";
+echo "\nФамилия и имя: $FIO\n";
